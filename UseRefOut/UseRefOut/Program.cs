013bf9c463649c6a9a5b0243d61587015e50975b
Aleksey <38381071+alekseychamin@users.noreply.github.com>
2018-04-18 20:37:40 +0300

    create decompose class and build

---
+++ UseRefOut/UseRefOut/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseRefOut
+{
+    class Program
+    {
+        static class Decompose
+        {
+            /* разделить числовое значение с плавающей точкой на целую и дробную часть */
+            public static int GetParts(double n, out double frac)
+            {
+                int whole = (int) n;
+                frac = n - whole;
+                return whole;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число : ");
+            string input = Console.ReadLine();
+            double n = Convert.ToDouble(input);            
+            int whole;
+            double frac;
+            whole = Decompose.GetParts(n, out frac);
+
+            Console.WriteLine("Целая часть = {0}; дробная часть числа = {1}", whole, frac);
+        }
+    }
+}
