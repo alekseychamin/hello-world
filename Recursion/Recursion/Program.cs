1ec18b9dc8dad9e14fd687f722ff648c2a0ce27f
Aleksey <38381071+alekseychamin@users.noreply.github.com>
2018-04-25 16:31:16 +0300

    add class Rec with method Fact for calculating factorial

--- Recursion/Recursion/Program.cs
+++ Recursion/Recursion/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace Recursion
 {
-    class Program
+    class Rec
     {
+        public int Fact(int n)
+        {
+            if (n == 1) return 1;
+            return Fact(n - 1) * n;
+        }
+    }
+    class Program
+    {        
         static void Main(string[] args)
         {
+            int n = 2;
+            Rec rec = new Rec();
+            Console.WriteLine("Factorial {0} = {1}", 2, rec.Fact(2).ToString());
+            Console.ReadLine();
         }
     }
 }
