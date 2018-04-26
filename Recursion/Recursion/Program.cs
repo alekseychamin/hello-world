d64d2773eba7fb906153fafcca72801d418dcea3
Aleksey <38381071+alekseychamin@users.noreply.github.com>
2018-04-26 18:04:43 +0300

    ThreeD first commit

--- Recursion/Recursion/Program.cs
+++ Recursion/Recursion/Program.cs
@@ -13,14 +13,32 @@ namespace Recursion
             if (n == 1) return 1;
             return Fact(n - 1) * n;
         }
+
+        public void Display(string str)
+        {
+            
+            Console.WriteLine(str);
+
+            if (str.Length > 0)
+                Display(str.Substring(1, str.Length - 1));
+            else return;
+
+            Console.Write(str[0]);
+
+
+        }
     }
     class Program
     {        
         static void Main(string[] args)
         {
             int n = 2;
+            string str = "test";
+
             Rec rec = new Rec();
             Console.WriteLine("Factorial {0} = {1}", 2, rec.Fact(2).ToString());
+            Console.WriteLine("Line = {0}", str);
+            rec.Display(str);
             Console.ReadLine();
         }
     }
