003c785c48f277e02757280ad91a095e7640ac68
Aleksey <38381071+alekseychamin@users.noreply.github.com>
2018-05-01 13:30:25 +0300

    add new example program for overload op

--- ThreeD/ThreeD/Program.cs
+++ ThreeD/ThreeD/Program.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace ThreeD
 {
-    class Program
+    class ThreeD
+    {
+        int x, y, z;
+
+        public ThreeD() { x = y = z = 0; }
+        public ThreeD(int x, int y, int z) { this.x = x;  this.y = y; this.z = z; }
+
+        public static ThreeD operator +(ThreeD ob1, ThreeD ob2)
+        {
+            ThreeD result = new ThreeD();
+            result.x = ob1.x + o
+        }
+    }
+
+    class ThreeDDemo
     {
         static void Main(string[] args)
         {
