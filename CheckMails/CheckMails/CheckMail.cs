237b211736c98747d40c5dade214308e8423125f
Aleksey <38381071+alekseychamin@users.noreply.github.com>
2018-05-01 20:56:40 +0300

    creta class CheckMail and properties

---
+++ CheckMails/CheckMails/CheckMail.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckMails
+{
+    class Mail
+    {
+        public string address;
+        public string password;
+    }
+    class BlackList
+    {
+        public string address;
+    }
+
+    class CheckMail
+    {
+        int uptime;
+        int period;
+
+        public List<Mail> ListMail = new List<Mail>();
+        public List<BlackList> ListBlack = new List<BlackList>();
+
+        public int UpTime
+        {
+            get
+            {
+                return uptime;                
+            }
+            set
+            {
+                if (UpTime > 1) uptime = UpTime;               
+            }
+        } 
+        public int Period
+        {
+            get
+            {
+                return period;
+            }
+            set
+            {
+                if (Period > 1) period = Period;
+            }
+        }
+
+        public void LoadConfig(string filename)
+        {
+
+        }
+    }
+}
