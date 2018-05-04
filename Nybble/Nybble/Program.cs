b5c20d92fed68b198dca58e79ac7ebdc04c83e03
Aleksey <38381071+alekseychamin@users.noreply.github.com>
2018-05-04 12:56:37 +0300

    add overload operator + 1. with two Nybble 2. with int, Nybble or Nybble, int add limit for storage 0-15

--- Nybble/Nybble/Program.cs
+++ Nybble/Nybble/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace Nybble
 {
-    class Program
+    class Nybble
     {
-        static void Main(string[] args)
+        public int Value;
+
+        public Nybble()
         {
+            Value = 0;
+        }
+
+        public Nybble(int value)
+        {
+            // ограничение на хранение данных 0-15
+            Value = (int) (value & (0x0000000F));
+        }
+
+        public static Nybble operator +(Nybble ob1, Nybble ob2)
+        {
+            return new Nybble { Value = ob1.Value + ob2.Value };
+        }
+
+        public static Nybble operator +(int ob1, Nybble ob2)
+        {
+            return new Nybble { Value = ob1 + ob2.Value };
+        }
+
+        public static Nybble operator +(Nybble ob1, int ob2)
+        {
+            return new Nybble { Value = ob1.Value + ob2 };
+        }
+
+    }
+    class NybbleDemo
+    {
+        static void Main()
+        {
+            Nybble a = new Nybble(15);
+            Nybble b = new Nybble(4);
+            Nybble c = a + b;
+            Console.WriteLine("a + b = {0} + {1} = {2}", a.Value, b.Value, c.Value);
+
+            Console.ReadLine();                       
         }
     }
 }
